21713874aa8c3eeb6c3a3e80ba4eb21aa7cae570
Valverde <fmatiaswilliam98@gmail.com>
2025-03-16 23:30:13 -0500

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -7,7 +7,7 @@ import "dotenv/config";
 const app = express();
 
 app.use(cors({ 
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: "https://emilia-project-prueba.vercel.app",
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
